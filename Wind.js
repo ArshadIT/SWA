c12c204e1cedcb161d243a2c827881261c4f27d0
ArshadIT <37140618+ArshadIT@users.noreply.github.com>
2020-09-15 20:28:11 +0200

    windjs

---
+++ Wind.js
@@ -0,0 +1,10 @@
+// wind 
+function wind(direction, mph, ms) {
+    const getDirection = () => direction;
+    const convertToMPH = () => ms;
+    const convertToMS = () => mph;
+return {
+    getDirection,
+    convertToMPH,
+    convertToMS
+}
