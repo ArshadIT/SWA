f3d403b8749544dd139d299dba8066f5fdedafe8
ArshadIT <37140618+ArshadIT@users.noreply.github.com>
2020-10-10 15:10:32 +0200

    examples ols codes

--- SWA_Assignment_2/Exercise_1.js
+++ SWA_Assignment_2/Exercise_1.js
@@ -1,76 +1,58 @@
-const pets = [
-    {type: 'dog', name:'Fido'}, 
-    {type: 'cat', name: 'Hannibal'}, 
-    {type: 'dog', name: 'Rover'},
-    {type: 'dragon', name: 'Fluffykins'}]
+// wind 
+function Wind(direction, number, date, place, type, unit) {
+    const getDirection = () => direction;
 
-const names = pets => {
-    let acc = []
-    for(let p of pets) {
-        acc.push(p.name) // p => p.name
+    function convertToMPH() {
+        return ((number / 1000) / 1.6093) * 3600;
     }
-    return acc
+
+    function convertToMS() {
+        return (((number * 1.6093) * 1000) / 60) / 60;
+    }
+    return Object.assign({ 
+        getDirection,
+        convertToMPH,
+        convertToMS
+    }, WeatherData(number, date, place, type, unit))
 }
 
-const types = pets => {
-    let acc = []
-    for(let p of pets) {
-        acc.push(p.type) // p => p.type
+
+// temperature
+function Temperature(number, date, place, type, unit) {
+    function convertToF() {
+        return number * 9 / 5 + 32;
     }
-    return acc
+
+    function convertToC() {
+        return (number - 32) * 5 / 9;
+    }
+    return Object.assign({
+        convertToF,
+        convertToC
+    }, WeatherData(number, date, place, type, unit))
 }
 
-console.log(names(pets))
-console.log(types(pets))
+// precipitation
 
-const map = (a, f) => {
-    let acc = []
-    for(let p of a) {
-        acc.push(f(p))
+function Precipitation(precType, number, date, place, type, unit) {
+    const precipitationType = () => precType;
+
+    function convertToInches() {
+        return number * 25.4;
     }
-    return acc
+
+    function convertToMM() {
+        return number / 25.4;
+    }
+    return Object.assign({
+        precipitationType,
+        convertToInches,
+        convertToMM
+    }, WeatherData(number, date, place, type, unit))
 }
 
-console.log(map(pets, p => p.name))
-console.log(map(pets, p => p.type))
+// cloud covarage
 
-const dogs = pets => {
-    let acc = []
-    for(let p of pets) {
-        if (p.type === 'dog') // p => p.type === 'dog'
-            acc.push(p)
-    }
-    return acc
+function CloudCovarage(number, date, place, type, unit) {
+    return Object.assign({}, WeatherData(number, date, place, type, unit))
 }
-
-const filter = (a, pred) => {
-    let acc = []
-    for(let p of a) {
-        if (pred(p)) // p => p.type === 'dog'
-            acc.push(p)
-    }
-    return acc
-}
-
-console.log(dogs(pets))
-console.log(filter(pets, p => p.type === 'dog'))
-
-const reduce = (a, f, init_value) => {
-    let acc = init_value
-    for(let p of a) {
-        acc = f(acc, p)
-    }
-    return acc
-}
-
-const map1 = (a, f) => reduce(a, (acc, v) => [...acc, f(v)], [])
-const filter1 = (a, p) => reduce(a, (acc, v) => p(v) ? [...acc, v] : acc, [])
-
-console.log(reduce([1, 2, 3, 4], (acc, v) => acc + v, 0))
-console.log(reduce([1, 2, 3, 4], (acc, v) => acc * v, 1))
-
-console.log()
-console.log(pets.map(p => p.type))
-console.log(pets.filter(p => p.type === 'dog'))
-console.log([1, 2, 3, 4].reduce((acc, v) => acc + v, 0))
-console.log([1, 2, 3, 4].reduce((acc, v) => acc * v, 1))
